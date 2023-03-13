6507772a88933afd1b2cb8cccbeab177c9f1c4fd
Dayany Lima Santos <dayanyylima@gmail.com>
2023-03-13 20:09:20 -0300

    Update

--- BookARoom/src/main/java/br/edu/ifnmg/bookaroom/campus/Campus.java
+++ BookARoom/src/main/java/br/edu/ifnmg/bookaroom/campus/Campus.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.edu.ifnmg.bookaroom.campus;
-
+import java.util.List;
 /**
  *
  * @author dayan
  */
 public class Campus {
+
+    private String nome;
+    private Endereco endereco;
+    private List<Predio> predios;
+
+    public Campus() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+   
 
 }
