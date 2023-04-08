29725119af602e061a12c69bef425637b3f10550
Dayany Lima Santos <dayanyylima@gmail.com>
2023-04-07 23:13:45 -0300

    update

--- BookARoom/src/main/java/br/edu/ifnmg/bookaroom/BookARoom.java
+++ BookARoom/src/main/java/br/edu/ifnmg/bookaroom/BookARoom.java
@@ -4,6 +4,7 @@
  */
 package br.edu.ifnmg.bookaroom;
 
+import br.edu.ifnmg.bookaroom.bancodedados.BancoDeDados;
 import br.edu.ifnmg.bookaroom.campus.Campus;
 import static br.edu.ifnmg.bookaroom.campus.ControladorCampus.listarCampus;
 import static br.edu.ifnmg.bookaroom.campus.ControladorCampus.pesquisarCampus;
@@ -37,16 +38,16 @@ import java.util.Scanner;
  * @author dayan
  */
 public class BookARoom {
-
-    private static List<Campus> listaCampus = new ArrayList<>();
+    
     private static List<ControladorReserva> controladores = new ArrayList<>();
 
     public static void menu() {
         System.out.println("Escolha uma das opcoes abaixo: \n");
-        System.out.println("0 - Sair.\n");
+        System.out.println("0 - Selecionar novo campus.\n");
         System.out.println("1 - Fazer reserva de sala.\n");
-        System.out.println("2 - Visualizar disponibilidade de salas.");
-        System.out.println("3 - Listar todas as reservas(ATIVAS/INATIVAS).");
+        System.out.println("2 - Visualizar disponibilidade de salas.\n");
+        System.out.println("3 - Listar todas as reservas(ATIVAS/INATIVAS).\n");
+        System.out.println("10 - Sair.\n");
     }
 
     public static void fazerReserva(ControladorReserva controlador) throws ParseException {
@@ -167,7 +168,7 @@ public class BookARoom {
             salasDisponiveis = controlador.consultarSalaDisponivel(dataReserva, horaInicio, horaFim);
             if (salasDisponiveis.isEmpty()) {
                 System.out.println("Não há salas disponíveis em dia e período informado.");
-                System.out.println("Deseja informar novo data e período para consulta? (1-sim ou 2-não)");
+                System.out.println("Deseja informar nova data e período para consulta? (1-sim ou 2-não)");
                 resposta1 = sc.nextInt();
 
             } else {
@@ -221,166 +222,82 @@ public class BookARoom {
 
         Scanner sc = new Scanner(System.in);
 
-        //<editor-fold defaultstate="collapsed" desc="CAMPUS MONTES CLAROS">
-        Predio predio1 = new Predio();
-        predio1.setNome("Predio 1");
+        BancoDeDados bd = new BancoDeDados();
+        bd.iniciarObjetos();
 
-        Sala sala1 = new Sala(1, 20, predio1);
-        Sala sala2 = new Sala(2, 20, predio1);
-        Sala sala3 = new Sala(3, 20, predio1);
-        Sala sala4 = new Sala(4, 20, predio1);
-
-        predio1.getSalas().add(sala1);
-        predio1.getSalas().add(sala2);
-        predio1.getSalas().add(sala3);
-        predio1.getSalas().add(sala4);
-
-        Predio predio2 = new Predio();
-        predio2.setNome("Predio 2");
-        Sala sala5 = new Sala(5, 20, predio2);
-        Sala sala6 = new Sala(6, 20, predio2);
-        Sala sala7 = new Sala(7, 20, predio2);
-        Sala sala8 = new Sala(8, 20, predio2);
-
-        predio2.getSalas().add(sala5);
-        predio2.getSalas().add(sala6);
-        predio2.getSalas().add(sala7);
-        predio2.getSalas().add(sala8);
-
-        Equipamento equipamento1 = new Equipamento(352394L, "Lousa Digital 1", "IFNMG");
-        Equipamento equipamento2 = new Equipamento(352395L, "Lousa Digital 2", "IFNMG");
-        Equipamento equipamento3 = new Equipamento(352396L, "Notebook Dell", "IFNMG");
-        Equipamento equipamento4 = new Equipamento(352381L, "Notebook Acer", "IFNMG");
-        Equipamento equipamento5 = new Equipamento(352382L, "Projetor 1", "IFNMG");
-        Equipamento equipamento6 = new Equipamento(352383L, "Projetor 2", "IFNMG");
-
-        Endereco endereco1 = new Endereco("Montes Claros", "Village do Lago I", "Rua Dois", 300);
-
-        Funcionario funcionario1 = new Funcionario("Amanda Silva", "Professora", "Educação");
-        Funcionario funcionario2 = new Funcionario("Ricardo Rocha", "Professor", "Educação");
-        Funcionario funcionario3 = new Funcionario("Sueli Santos", "Professora", "Educação");
-        Funcionario funcionario4 = new Funcionario("Antonio Gomes", "Professor", "Educação");
-
-        Campus campus1 = new Campus();
-        campus1.setNome("Campus Montes Claros");
-        campus1.setEndereco(endereco1);
-        campus1.getPredios().add(predio1);
-        campus1.getPredios().add(predio2);
-        campus1.getEquipamentos().add(equipamento1);
-        campus1.getEquipamentos().add(equipamento2);
-        campus1.getEquipamentos().add(equipamento3);
-        campus1.getEquipamentos().add(equipamento4);
-        campus1.getEquipamentos().add(equipamento5);
-        campus1.getEquipamentos().add(equipamento6);
-
-        campus1.getFuncionarios().add(funcionario1);
-        campus1.getFuncionarios().add(funcionario2);
-        campus1.getFuncionarios().add(funcionario3);
-        campus1.getFuncionarios().add(funcionario4);
-        //</editor-fold>
-
-        //<editor-fold defaultstate="collapsed" desc="CAMPUS PIRAPORA"> 
-        Predio predio3 = new Predio();
-        predio3.setNome("Predio 1");
-        Sala sala9 = new Sala(1, 30, predio3);
-        Sala sala10 = new Sala(2, 30, predio3);
-        Sala sala11 = new Sala(3, 30, predio3);
-        Sala sala12 = new Sala(4, 30, predio3);
-        Sala sala13 = new Sala(5, 30, predio3);
-        Sala sala14 = new Sala(6, 30, predio3);
-
-        predio3.getSalas().add(sala9);
-        // predio3.getSalas().add(sala10);
-//        predio3.getSalas().add(sala11);
-//        predio3.getSalas().add(sala12);
-//        predio3.getSalas().add(sala13);
-//        predio3.getSalas().add(sala14);
-
-        Equipamento equipamento7 = new Equipamento(567881L, "Lousa Digital 1", "IFNMG");
-        Equipamento equipamento8 = new Equipamento(567882L, "Lousa Digital 2", "IFNMG");
-        Equipamento equipamento9 = new Equipamento(567883L, "Notebook Asus", "IFNMG");
-        Equipamento equipamento10 = new Equipamento(567884L, "Notebook Lenovo", "IFNMG");
-        Equipamento equipamento11 = new Equipamento(567885L, "Projetor 1", "IFNMG");
-        Equipamento equipamento12 = new Equipamento(567886L, "Projetor 2", "IFNMG");
-
-        Endereco endereco2 = new Endereco("Pirapora", "Santos Dumont", "Av. Humberto Mallard", 1355);
-
-        Funcionario funcionario5 = new Funcionario("Adriana Silva", "Professora", "Educação");
-        Funcionario funcionario6 = new Funcionario("Paulo Rocha", "Professor", "Educação");
-        Funcionario funcionario7 = new Funcionario("Laura Santos", "Professora", "Educação");
-        Funcionario funcionario8 = new Funcionario("Flávio Gomes", "Professor", "Educação");
-
-        Campus campus2 = new Campus();
-        campus2.setNome("Campus Pirapora");
-        campus2.setEndereco(endereco2);
-        campus2.getPredios().add(predio3);
-        campus2.getEquipamentos().add(equipamento7);
-        campus2.getEquipamentos().add(equipamento8);
-        campus2.getEquipamentos().add(equipamento9);
-        campus2.getEquipamentos().add(equipamento10);
-        campus2.getEquipamentos().add(equipamento11);
-        campus2.getEquipamentos().add(equipamento12);
-
-        campus2.getFuncionarios().add(funcionario5);
-        campus2.getFuncionarios().add(funcionario6);
-        campus2.getFuncionarios().add(funcionario7);
-        campus2.getFuncionarios().add(funcionario8);
-
-        //</editor-fold>
-        listaCampus.add(campus1);
-        listaCampus.add(campus2);
-        ControladorReserva controlador1 = new ControladorReserva();
-        controlador1.setCampus(campus1);
-        ControladorReserva controlador2 = new ControladorReserva();
-        controlador2.setCampus(campus2);
-        controladores.add(controlador1);
-        controladores.add(controlador2);
+        for (Campus c : bd.getListaCampus()) {
+            ControladorReserva controlador = new ControladorReserva();
+            controlador.setCampus(c);
+            controladores.add(controlador);
+        }
 
         System.out.println("Bem vindo ao Sistema de Reserva de Salas!\n");
 
         int opcao;
-
-        System.out.println("Por favor informe campus que deseja realizar reserva de sala:\n");
-
-        listarCampus(listaCampus);
-
-        System.out.print("Digite: ");
-        String campusNome = sc.nextLine();
-
-        Campus campusSelecionado = new Campus();
-
-        campusSelecionado = pesquisarCampus(campusNome, listaCampus);
-
-        while (campusSelecionado == null) {
-
-            System.out.print("Digite nome de campus válido: ");
-            campusNome = sc.nextLine();
-            campusSelecionado = pesquisarCampus(campusNome, listaCampus);
-        }
-
-        ControladorReserva controlador = new ControladorReserva(campusSelecionado);
-        System.out.println(campusSelecionado.toString());
+        String sair = "n";
 
         do {
-            menu();
+            System.out.println("Por favor informe campus que deseja realizar reserva de sala:\n");
 
-            System.out.print("Opção: ");
-            opcao = sc.nextInt();
+            listarCampus(bd.getListaCampus());
 
-            switch (opcao) {
+            
+            System.out.print("Digite: ");
+            String campusNome = sc.nextLine();
 
-                case 1:
-                    fazerReserva(controlador);
-                    break;
-                case 2:
-                    consultarDisponibilidadeDeSalas(controlador);
-                    break;
-                case 3:
-                    consultarReservasUsuarios(controlador);
-                    break;
+            Campus campusSelecionado = new Campus();
+
+            campusSelecionado = pesquisarCampus(campusNome, bd.getListaCampus());
+
+            while (campusSelecionado == null) {
+
+                System.out.print("Digite nome de campus válido: ");
+                campusNome = sc.nextLine();
+                campusSelecionado = pesquisarCampus(campusNome, bd.getListaCampus());
             }
 
-        } while (opcao != 0);
+            ControladorReserva controlador = new ControladorReserva();
+            System.out.println(campusSelecionado.toString());
+            int i = 0;
+            for (ControladorReserva ctrls : controladores) {
+
+                if (controladores.get(i).getCampus().equals(campusSelecionado)) {
+                    controlador = ctrls;
+                }
+                i++;
+            }
+
+            do {
+                menu();
+
+                System.out.print("Opção: ");
+                opcao = sc.nextInt();
+
+                switch (opcao) {
+
+                    case 1:
+                        fazerReserva(controlador);
+                        break;
+                    case 2:
+                        consultarDisponibilidadeDeSalas(controlador);
+                        break;
+                    case 3:
+                        consultarReservasUsuarios(controlador);
+                        break;
+                    case 0:
+                        break;
+                    case 10:
+                        sair = "s";
+                        break;
+
+                }
+
+                if (sair.equalsIgnoreCase("s")) {
+                    break;
+                }
+
+            } while (opcao != 0);
+        } while (sair.equalsIgnoreCase("n"));
 
     }
 }
---
+++ BookARoom/src/main/java/br/edu/ifnmg/bookaroom/bancodedados/BancoDeDados.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.bookaroom.bancodedados;
+
+import br.edu.ifnmg.bookaroom.campus.Campus;
+import br.edu.ifnmg.bookaroom.endereco.Endereco;
+import br.edu.ifnmg.bookaroom.equipamento.Equipamento;
+import br.edu.ifnmg.bookaroom.funcionario.Funcionario;
+import br.edu.ifnmg.bookaroom.predio.Predio;
+import br.edu.ifnmg.bookaroom.sala.Sala;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author dayan
+ */
+public class BancoDeDados {
+
+    private List<Campus> listaCampus = new ArrayList<>();
+
+    public List<Campus> getListaCampus() {
+        return listaCampus;
+    }
+   
+    public void iniciarObjetos() {
+
+        Predio predio1 = new Predio();
+        predio1.setNome("Predio 1");
+
+        Sala sala1 = new Sala(1, 20, predio1);
+        Sala sala2 = new Sala(2, 20, predio1);
+        Sala sala3 = new Sala(3, 20, predio1);
+        Sala sala4 = new Sala(4, 20, predio1);
+
+        predio1.getSalas().add(sala1);
+        predio1.getSalas().add(sala2);
+        predio1.getSalas().add(sala3);
+        predio1.getSalas().add(sala4);
+
+        Predio predio2 = new Predio();
+        predio2.setNome("Predio 2");
+        Sala sala5 = new Sala(5, 20, predio2);
+        Sala sala6 = new Sala(6, 20, predio2);
+        Sala sala7 = new Sala(7, 20, predio2);
+        Sala sala8 = new Sala(8, 20, predio2);
+
+        predio2.getSalas().add(sala5);
+        predio2.getSalas().add(sala6);
+        predio2.getSalas().add(sala7);
+        predio2.getSalas().add(sala8);
+
+        Equipamento equipamento1 = new Equipamento(352394L, "Lousa Digital 1", "IFNMG");
+        Equipamento equipamento2 = new Equipamento(352395L, "Lousa Digital 2", "IFNMG");
+        Equipamento equipamento3 = new Equipamento(352396L, "Notebook Dell", "IFNMG");
+        Equipamento equipamento4 = new Equipamento(352381L, "Notebook Acer", "IFNMG");
+        Equipamento equipamento5 = new Equipamento(352382L, "Projetor 1", "IFNMG");
+        Equipamento equipamento6 = new Equipamento(352383L, "Projetor 2", "IFNMG");
+
+        Endereco endereco1 = new Endereco("Montes Claros", "Village do Lago I", "Rua Dois", 300);
+
+        Funcionario funcionario1 = new Funcionario("Amanda Silva", "Professora", "Educação");
+        Funcionario funcionario2 = new Funcionario("Ricardo Rocha", "Professor", "Educação");
+        Funcionario funcionario3 = new Funcionario("Sueli Santos", "Professora", "Educação");
+        Funcionario funcionario4 = new Funcionario("Antonio Gomes", "Professor", "Educação");
+
+        Campus campus1 = new Campus();
+        campus1.setNome("Campus Montes Claros");
+        campus1.setEndereco(endereco1);
+        campus1.getPredios().add(predio1);
+        campus1.getPredios().add(predio2);
+        campus1.getEquipamentos().add(equipamento1);
+        campus1.getEquipamentos().add(equipamento2);
+        campus1.getEquipamentos().add(equipamento3);
+        campus1.getEquipamentos().add(equipamento4);
+        campus1.getEquipamentos().add(equipamento5);
+        campus1.getEquipamentos().add(equipamento6);
+
+        campus1.getFuncionarios().add(funcionario1);
+        campus1.getFuncionarios().add(funcionario2);
+        campus1.getFuncionarios().add(funcionario3);
+        campus1.getFuncionarios().add(funcionario4);
+        //</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="CAMPUS PIRAPORA"> 
+        Predio predio3 = new Predio();
+        predio3.setNome("Predio 1");
+        Sala sala9 = new Sala(1, 30, predio3);
+        Sala sala10 = new Sala(2, 30, predio3);
+        Sala sala11 = new Sala(3, 30, predio3);
+        Sala sala12 = new Sala(4, 30, predio3);
+        Sala sala13 = new Sala(5, 30, predio3);
+        Sala sala14 = new Sala(6, 30, predio3);
+
+        predio3.getSalas().add(sala9);
+        predio3.getSalas().add(sala10);
+        predio3.getSalas().add(sala11);
+        predio3.getSalas().add(sala12);
+        predio3.getSalas().add(sala13);
+        predio3.getSalas().add(sala14);
+
+        Equipamento equipamento7 = new Equipamento(567881L, "Lousa Digital 1", "IFNMG");
+        Equipamento equipamento8 = new Equipamento(567882L, "Lousa Digital 2", "IFNMG");
+        Equipamento equipamento9 = new Equipamento(567883L, "Notebook Asus", "IFNMG");
+        Equipamento equipamento10 = new Equipamento(567884L, "Notebook Lenovo", "IFNMG");
+        Equipamento equipamento11 = new Equipamento(567885L, "Projetor 1", "IFNMG");
+        Equipamento equipamento12 = new Equipamento(567886L, "Projetor 2", "IFNMG");
+
+        Endereco endereco2 = new Endereco("Pirapora", "Santos Dumont", "Av. Humberto Mallard", 1355);
+
+        Funcionario funcionario5 = new Funcionario("Adriana Silva", "Professora", "Educação");
+        Funcionario funcionario6 = new Funcionario("Paulo Rocha", "Professor", "Educação");
+        Funcionario funcionario7 = new Funcionario("Laura Santos", "Professora", "Educação");
+        Funcionario funcionario8 = new Funcionario("Flávio Gomes", "Professor", "Educação");
+
+        Campus campus2 = new Campus();
+        campus2.setNome("Campus Pirapora");
+        campus2.setEndereco(endereco2);
+        campus2.getPredios().add(predio3);
+        campus2.getEquipamentos().add(equipamento7);
+        campus2.getEquipamentos().add(equipamento8);
+        campus2.getEquipamentos().add(equipamento9);
+        campus2.getEquipamentos().add(equipamento10);
+        campus2.getEquipamentos().add(equipamento11);
+        campus2.getEquipamentos().add(equipamento12);
+
+        campus2.getFuncionarios().add(funcionario5);
+        campus2.getFuncionarios().add(funcionario6);
+        campus2.getFuncionarios().add(funcionario7);
+        campus2.getFuncionarios().add(funcionario8);
+         //</editor-fold>
+        listaCampus.add(campus1);
+        listaCampus.add(campus2);
+
+    }
+
+}
